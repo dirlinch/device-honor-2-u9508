306b210c91400935623d9430632ad683476fd70e
dirlinch <megatort@mail.ru>
2014-02-23 01:05:24 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,8 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-
-add_lunch_combo hwu9508-eng
 add_lunch_combo hwu9508-userdebug
-add_lunch_combo hwu9508-user
-add_lunch_combo hwu9508-tests
